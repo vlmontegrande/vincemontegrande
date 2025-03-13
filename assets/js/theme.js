fd3341903d8f678617e253ffe03f09b04c9e7184
vlmontegrande <vlmontegrande@gmail.com>
2025-03-12 20:06:38 -0700

    Remove DOMContentLoaded event from theme toggle

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,21 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.querySelector('#theme-toggle');
-    themeToggle.style.display = 'block';
+const themeToggle = document.querySelector('#theme-toggle');
+themeToggle.style.display = 'block';
+
+const savedTheme = localStorage.getItem('theme');
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (savedTheme === 'dark' || (savedTheme === null && prefersDarkScheme)) {
+    document.documentElement.classList.add('dark-theme');
+}
+
+const toggleTheme = () => {
+    const isDarkTheme = document.documentElement.classList.toggle('dark-theme');
     
-    const savedTheme = localStorage.getItem('theme');
-    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    themeToggle.innerHTML = isDarkTheme ? sunIcon : moonIcon;
     
-    if (savedTheme === 'dark' || (savedTheme === null && prefersDarkScheme)) {
-        document.documentElement.classList.add('dark-theme');
-    }
-    
-    const toggleTheme = () => {
-        const isDarkTheme = document.documentElement.classList.toggle('dark-theme');
-        
-        themeToggle.innerHTML = isDarkTheme ? sunIcon : moonIcon;
-        
-        localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
-    };
-    
-    themeToggle.addEventListener('click', toggleTheme);
-});
+    localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
+};
+
+themeToggle.addEventListener('click', toggleTheme);

a4f294037f21837cd93c6f9f852bc2ff6ee01a68
vlmontegrande <vlmontegrande@gmail.com>
2025-03-16 12:14:10 -0700

    Fix theme toggle so browser remembers theme preference

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -8,12 +8,9 @@ if (savedTheme === 'dark' || (savedTheme === null && prefersDarkScheme)) {
     document.documentElement.classList.add('dark-theme');
 }
 
-const toggleTheme = () => {
+function toggleTheme() {
     const isDarkTheme = document.documentElement.classList.toggle('dark-theme');
-    
-    themeToggle.innerHTML = isDarkTheme ? sunIcon : moonIcon;
-    
     localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
-};
+}
 
 themeToggle.addEventListener('click', toggleTheme);
